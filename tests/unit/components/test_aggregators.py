84f676535f48035bdb4a2e6911c203136df4a46d
vlreinier <43336873+vlreinier@users.noreply.github.com>
2025-12-24 09:42:38 +0100

    Delete tests directory

--- tests/unit/components/test_aggregators.py
+++
@@ -1,3 +0,0 @@
-# This file is intentionally left blank.
-# The tests for batch and reduce, which were previously here, have been
-# moved to test_generators.py as they are no longer aggregator stages.
